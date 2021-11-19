9aaeb6ff424fb08927d0b53a0095978b4fc20a8f
Eoin Pinaqui <pinaquie@tcd.ie>
2021-11-19 16:50:01 +0000

    made it nicer

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 # constants.py
 # This file contains useful constants that are used throughout the project.
 
-DEFAULT_REGION = 'euw1'
+DEFAULT_URL_REGION = 'euw1'
+DEFAULT_REGION = 'europe'
 BASE_URL = 'api.riotgames.com'
-API_KEY = 'RGAPI-4a5950f7-ac9d-4241-8e13-bf771e4ded7d'
+API_KEY = 'RGAPI-3b29333f-86ff-467d-a7ae-330e55f59e72'
--- scrape.py
+++ scrape.py
@@ -1,31 +1,21 @@
 # scrape.py
 # This file contains many useful functions for gathering data from the Riot API.
 
-import requests
 import constants
 import json
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import LolWatcher
 
+# Create the league watcher object (handles rate limiting when scraping data)
 watcher = LolWatcher(constants.API_KEY)
 
+# Get information about a given summoner
+summoner = watcher.summoner.by_name(constants.DEFAULT_URL_REGION, 'pinaquack')
 
-# Get data about a summoner
-def get_summoner(summoner_name, region=constants.DEFAULT_REGION, api_key=constants.API_KEY):
-    return LolWatcher(api_key).summoner.by_name(region, summoner_name)
+# Get the match history for a given summoner
+matches = watcher.match.matchlist_by_puuid(constants.DEFAULT_REGION, summoner['puuid'])
 
+# Get the information for the first match in the list
+match = watcher.match.by_id(constants.DEFAULT_REGION, matches[0])
 
-# Get a list of matches from a summoner puuid
-def get_match_list_for_summoner(summoner_puuid, region=constants.DEFAULT_REGION, api_key=constants.API_KEY):
-    return LolWatcher(api_key).match.matchlist_by_puuid(region, summoner_puuid)
-
-
-# Get a match by its id
-def get_match_by_id(match_id, region=constants.DEFAULT_REGION, api_key=constants.API_KEY):
-    return LolWatcher(api_key).match.by_id(region, match_id)
-
-
-pin = get_summoner('pinaquack')
-matches = get_match_list_for_summoner(pin['puuid'], region='europe')
-match = get_match_by_id(matches[0], region='europe')
-
-print(json.dumps(match, sort_keys=False, indent=4))
+with open('match.json', 'w', encoding='utf-8') as f:
+    json.dump(match, f, sort_keys=False, ensure_ascii=False, indent=4)
