214ab65f9699131d922e75ddf09c7ef1209ddb8c
Eoin Pinaqui <pinaquie@tcd.ie>
2021-11-19 17:28:31 +0000

    Getting the challengers

--- constants.py
+++ constants.py
@@ -1,7 +1,34 @@
 # constants.py
 # This file contains useful constants that are used throughout the project.
 
-DEFAULT_URL_REGION = 'euw1'
-DEFAULT_REGION = 'europe'
+# Base API information
 BASE_URL = 'api.riotgames.com'
 API_KEY = 'RGAPI-3b29333f-86ff-467d-a7ae-330e55f59e72'
+
+# Dataset directory structure
+DATASET_DIRECTORIES = [
+    './dataset',
+    './dataset/challenger_players'
+]
+
+# All the url routing regions used by the riot api
+URL_REGIONS = [
+    'br1',
+    'eun1',
+    'euw1',
+    'jp1',
+    'kr',
+    'la1',
+    'la2',
+    'na1',
+    'oc1',
+    'tr1',
+    'ru'
+]
+
+# All the regional routing regions used by the riot api
+REGIONS = [
+    'AMERICAS',
+    'ASIA',
+    'EUROPE'
+]
--- scrape.py
+++ scrape.py
@@ -1,21 +1,49 @@
 # scrape.py
 # This file contains many useful functions for gathering data from the Riot API.
 
-import constants
-import json
+import csv
+import os
+
 from riotwatcher import LolWatcher
+
+import constants
 
 # Create the league watcher object (handles rate limiting when scraping data)
 watcher = LolWatcher(constants.API_KEY)
 
-# Get information about a given summoner
-summoner = watcher.summoner.by_name(constants.DEFAULT_URL_REGION, 'pinaquack')
 
-# Get the match history for a given summoner
-matches = watcher.match.matchlist_by_puuid(constants.DEFAULT_REGION, summoner['puuid'])
+# Get a list of all the challenger players in a given region
+# challengers = watcher.league.challenger_by_queue(constants.DEFAULT_URL_REGION, 'RANKED_SOLO_5x5')
 
-# Get the information for the first match in the list
-match = watcher.match.by_id(constants.DEFAULT_REGION, matches[0])
+# Function to create the directory structure to hold the data set
+def create_dataset_structure():
+    for directory in constants.DATASET_DIRECTORIES:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-with open('match.json', 'w', encoding='utf-8') as f:
-    json.dump(match, f, sort_keys=False, ensure_ascii=False, indent=4)
+
+# Function to get all the challenger players from the riot api
+def get_challenger_players():
+    # Loop through all of the regions
+    for url_region in constants.URL_REGIONS:
+
+        # Get the list of challenger players from teh riot api
+        challengers = watcher.league.challenger_by_queue(url_region, 'RANKED_SOLO_5x5')
+
+        # Open a new csv file to write the data to
+        csv_file = open('./dataset/challenger_players/challenger_players.' + url_region + '.csv', 'w', encoding='utf-8',
+                        newline='')
+        csv_writer = csv.writer(csv_file)
+
+        # Write the data to the open csv
+        write_headers = True
+        for player in challengers['entries']:
+            if write_headers:
+                csv_writer.writerow(player.keys())
+                write_headers = False
+            csv_writer.writerow(player.values())
+        csv_file.close()
+
+
+create_dataset_structure()
+get_challenger_players()
