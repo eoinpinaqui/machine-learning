2ce3b81400b05faf0c1ffce637d45b7e25d1ecf8
Eoin Pinaqui <pinaquie@tcd.ie>
2021-11-22 15:42:19 +0000

    Scraped some data

--- constants.py
+++ constants.py
@@ -3,12 +3,13 @@
 
 # Base API information
 BASE_URL = 'api.riotgames.com'
-API_KEY = 'RGAPI-3b29333f-86ff-467d-a7ae-330e55f59e72'
+API_KEY = 'RGAPI-4177431d-41b0-4f13-a79b-359cbda2d9aa'
 
 # Dataset directory structure
 DATASET_DIRECTORIES = [
     './dataset',
-    './dataset/challenger_players'
+    './dataset/challenger_players',
+    './dataset/challenger_games'
 ]
 
 # All the url routing regions used by the riot api
@@ -27,8 +28,104 @@ URL_REGIONS = [
 ]
 
 # All the regional routing regions used by the riot api
-REGIONS = [
-    'AMERICAS',
-    'ASIA',
-    'EUROPE'
+REGIONS = {
+    'br1': 'americas',
+    'eun1': 'europe',
+    'euw1': 'europe',
+    'jp1': 'asia',
+    'kr': 'asia',
+    'la1': 'americas',
+    'la2': 'americas',
+    'na1': 'americas',
+    'oc1': 'asia',
+    'tr1': 'europe',
+    'ru': 'europe'
+}
+
+# Challenger games dataset headers
+GAME_HEADERS = [
+    # Blue team top
+    'champ1.1',
+    'kills1.1',
+    'assists1.1',
+    'deaths1.1',
+    'gold1.1',
+
+    # Blue team jungle
+    'champ1.2',
+    'kills1.2',
+    'assists1.2',
+    'deaths1.2',
+    'gold1.2',
+
+    # Blue team mid
+    'champ1.3',
+    'kills1.3',
+    'assists1.3',
+    'deaths1.3',
+    'gold1.3',
+
+    # Blue team bot
+    'champ1.4',
+    'kills1.4',
+    'assists1.4',
+    'deaths1.4',
+    'gold1.4',
+
+    # Blue team supp
+    'champ1.5',
+    'kills1.5',
+    'assists1.5',
+    'deaths1.5',
+    'gold1.5',
+
+    # Red team top
+    'champ2.1',
+    'kills2.1',
+    'assists2.1',
+    'deaths2.1',
+    'gold2.1',
+
+    # Red team jungle
+    'champ2.2',
+    'kills2.2',
+    'assists2.2',
+    'deaths2.2',
+    'gold2.2',
+
+    # Red team mid
+    'champ2.3',
+    'kills2.3',
+    'assists2.3',
+    'deaths2.3',
+    'gold2.3',
+
+    # Red team bot
+    'champ2.4',
+    'kills2.4',
+    'assists2.4',
+    'deaths2.4',
+    'gold2.4',
+
+    # Red team supp
+    'champ1.5',
+    'kills1.5',
+    'assists1.5',
+    'deaths1.5',
+    'gold1.5',
+
+    # Objectives
+    'first blood',
+    'first tower',
+    'blue towers',
+    'red towers',
+    'blue dragons',
+    'red dragons',
+    'blue heralds',
+    'red heralds',
+    'blue barons',
+    'red barons',
+    
+    # Target
+    'win'
 ]
--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 # scrape.py
 # This file contains many useful functions for gathering data from the Riot API.
 
+import pandas as pd
+from tqdm import tqdm
 import csv
 import os
 
@@ -20,27 +22,85 @@ def create_dataset_structure():
 
 
 # Function to get all the challenger players from the riot api
-def get_challenger_players():
-    # Loop through all of the regions
-    for url_region in constants.URL_REGIONS:
+def get_challenger_players(region):
+    # Get the list of challenger players from teh riot api
+    challengers = watcher.league.challenger_by_queue(region, 'RANKED_SOLO_5x5')
 
-        # Get the list of challenger players from teh riot api
-        challengers = watcher.league.challenger_by_queue(url_region, 'RANKED_SOLO_5x5')
+    # Open a new csv file to write the data to
+    csv_file = open('./dataset/challenger_players/challenger_players.' + region + '.csv', 'w',
+                    encoding='utf-8', newline='')
+    csv_writer = csv.writer(csv_file)
 
-        # Open a new csv file to write the data to
-        csv_file = open('./dataset/challenger_players/challenger_players.' + url_region + '.csv', 'w',
-                        encoding='utf-8', newline='')
-        csv_writer = csv.writer(csv_file)
-
-        # Write the data to the open csv
-        write_headers = True
-        for player in challengers['entries']:
-            if write_headers:
-                csv_writer.writerow(player.keys())
-                write_headers = False
-            csv_writer.writerow(player.values())
-        csv_file.close()
+    # Write the data to the open csv
+    write_headers = True
+    for player in challengers['entries']:
+        if write_headers:
+            csv_writer.writerow(player.keys())
+            write_headers = False
+        csv_writer.writerow(player.values())
+    csv_file.close()
 
 
-create_dataset_structure()
-get_challenger_players()
+# Function to get challenger games from the riot api
+# considers most recent n games of the challenger players in a region
+def get_challenger_games(region, n):
+    # Get all of the challenger players for the given region
+    create_dataset_structure()
+    get_challenger_players(region)
+    players = pd.read_csv('./dataset/challenger_players/challenger_players.' + region + '.csv').values.tolist()
+
+    # Loop through each player and get their most recent n games
+    region_games = {}
+    for i in tqdm(range(len(players))):
+        player = players[i]
+        try:
+            p = watcher.summoner.by_name(region, player[1])
+            games = watcher.match.matchlist_by_puuid(constants.REGIONS[region], p['puuid'], count=n)
+            for game in games:
+                region_games[game] = get_game_info(constants.REGIONS[region], game)
+        except:
+            print("Couldn't find " + player[1])
+
+    # Open a new csv file to write the data to
+    csv_file = open('./dataset/challenger_games/challenger_games.' + region + '.csv', 'w',
+                    encoding='utf-8', newline='')
+    csv_writer = csv.writer(csv_file)
+
+    # Write the data to the open csv
+    csv_writer.writerow(constants.GAME_HEADERS)
+    for game in region_games:
+        csv_writer.writerow(region_games[game])
+
+    csv_file.close()
+
+
+def get_game_info(region, game_id):
+    game = watcher.match.by_id(region, game_id)['info']
+    result = []
+    for x in game['participants']:
+        result.append(x['championId'])
+        result.append(x['kills'])
+        result.append(x['assists'])
+        result.append(x['deaths'])
+        result.append(x['goldEarned'])
+
+    blue_team = game['teams'][0]
+    red_team = game['teams'][1]
+
+    result.append(1 * blue_team['objectives']['champion']['first'] + -1 * red_team['objectives']['champion']['first'])
+    result.append(1 * blue_team['objectives']['tower']['first'] + -1 * red_team['objectives']['tower']['first'])
+    result.append(blue_team['objectives']['tower']['kills'])
+    result.append(red_team['objectives']['tower']['kills'])
+    result.append(blue_team['objectives']['dragon']['kills'])
+    result.append(red_team['objectives']['dragon']['kills'])
+    result.append(blue_team['objectives']['riftHerald']['kills'])
+    result.append(red_team['objectives']['riftHerald']['kills'])
+    result.append(blue_team['objectives']['baron']['kills'])
+    result.append(red_team['objectives']['baron']['kills'])
+
+    result.append(1 * blue_team['win'] + -1 * red_team['win'])
+
+    return result
+
+
+get_challenger_games('euw1', 10)
