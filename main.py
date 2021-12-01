fbf7ae051af7d818feeef71d001a22844030820b
Eoin Pinaqui <pinaquie@tcd.ie>
2021-12-01 23:15:05 +0000

    Added linear-svc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +9,7 @@ from sklearn.metrics import confusion_matrix, roc_curve
 
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
 '''
 Utility Functions
@@ -67,6 +70,34 @@ def cross_validate_logistic_regression(features, targets):
     print(means)
 
 
+def cross_validate_linear_svc(features, targets):
+    C_range = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+
+    means, stds, models = [], [], []
+
+    for C in C_range:
+        scores = cross_val_score(LinearSVC(C=C), features, targets, cv=5, scoring='accuracy')
+        means.append(scores.mean())
+        stds.append(scores.std())
+
+    br = cross_validate_baseline_random_score(features, targets)
+    bf = cross_validate_baseline_most_frequent_score(features, targets)
+
+    plt.figure(figsize=(10, 10))
+    plt.errorbar(C_range, [br['mean']] * len(C_range), yerr=[br['std']] * len(C_range), fmt='y')
+    plt.errorbar(C_range, [bf['mean']] * len(C_range), yerr=[bf['std']] * len(C_range), fmt='r')
+    plt.errorbar(C_range, means, yerr=stds, fmt='b')
+    plt.ylabel('Mean Accuracy Score')
+    plt.xlabel('C')
+    plt.xscale('log')
+    plt.title('Comparing accuracy of baseline models with Linear SVC model')
+    plt.legend(['Baseline (random)', 'Baseline (most frequent)', 'Trained model'])
+    plt.show()
+
+    print('Mean Accuracy Scores for Linear SVC')
+    print(means)
+
+
 '''
 Model Training Functions
 '''
@@ -94,7 +125,7 @@ def logistic_regression(features, targets):
     X_train, X_test, Y_train, Y_test = train_test_split(features, targets, test_size=0.2)
     model = LogisticRegression(C=1, penalty='l2', solver='lbfgs').fit(X_train, Y_train)
     Y_pred = model.predict(X_test)
-    print('Confusion Matrix for Baseline Classifier (Most Frequent)')
+    print('Confusion Matrix for Logistic Regression Model')
     print(confusion_matrix(Y_test, Y_pred))
 
     cm_rand = confusion_matrix_random(features, targets)
@@ -115,20 +146,68 @@ def logistic_regression(features, targets):
     plt.show()
 
 
+def linear_svc(features, targets):
+    X_train, X_test, Y_train, Y_test = train_test_split(features, targets, test_size=0.2)
+    model = LinearSVC(C=0.1).fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+    print('Confusion Matrix for Lienar SVC model')
+    print(confusion_matrix(Y_test, Y_pred))
+
+    cm_rand = confusion_matrix_random(features, targets)
+    cm_freq = confusion_matrix_most_frequent(features, targets)
+
+    fpr, tpr, _ = roc_curve(Y_test, model.decision_function(X_test))
+    plt.plot(fpr, tpr)
+    rand_tpr = cm_rand[1][1] / (cm_rand[1][1] + cm_rand[1][0])
+    rand_fpr = cm_rand[0][1] / (cm_rand[0][1] + cm_rand[0][0])
+    freq_tpr = cm_freq[1][1] / (cm_freq[1][1] + cm_freq[1][0])
+    freq_fpr = cm_freq[0][1] / (cm_freq[0][1] + cm_freq[0][0])
+    plt.plot(rand_tpr, rand_fpr, 'rs')
+    plt.plot(freq_tpr, freq_fpr, 'gs')
+    plt.title('ROC Curve for Linear SVC Model')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.legend(['Trained model', 'Baseline (random)', 'Baseline (most frequent)'])
+    plt.show()
+
+
 '''
 Main
 '''
 
 
 def main():
+    error = False
+    model = ''
+
+    if len(sys.argv) < 2:
+        print('You did not specify a model type.')
+        error = True
+    else:
+        model = str(sys.argv[1])
+
     games = pd.read_csv('dataset/challenger_games/challenger_games_big.euw1.csv').values.tolist()
     X = get_features(games, len(games[0]) - 1)
     Y = get_targets(games)
 
-    cross_validate_logistic_regression(X, Y)
-    confusion_matrix_random(X, Y)
-    confusion_matrix_most_frequent(X, Y)
-    logistic_regression(X, Y)
+    if model == 'logistic-regression':
+        cross_validate_logistic_regression(X, Y)
+        confusion_matrix_random(X, Y)
+        confusion_matrix_most_frequent(X, Y)
+        logistic_regression(X, Y)
+    elif model == 'linear-svc':
+        cross_validate_linear_svc(X, Y)
+        confusion_matrix_random(X, Y)
+        confusion_matrix_most_frequent(X, Y)
+        linear_svc(X, Y)
+    elif not error:
+        print('Specified model is not supported.')
+        error = True
+
+    if error:
+        print('The current model types are currently supported:')
+        print('    - Logistic Regression (python main.py logistic-regression)')
+        print('    - Linear SVC          (python main.py linear-svc)')
 
 
 if __name__ == "__main__":
