ed46cb00b8d1711e51f80b99f5191f158021e06d
Eoin Pinaqui <pinaquie@tcd.ie>
2021-12-01 17:38:09 +0000

    logistic regression

--- main.py
+++ main.py
@@ -2,129 +2,138 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve
 
-from sklearn.linear_model import LogisticRegression, Lasso, Ridge
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.dummy import DummyRegressor
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression
 
-##
-## --------------------------------
-##        UTLITY FUNCTIONS
-##---------------------------------
-##
-
-# Will return data from a specified column
-def getDataFromColumn(data, column):
-    return [i[column] for i in data]
-
-# Will return a column_stack of the amount of features specified
-def getFeatures(data, n):
-    ## intialise with first feature from first column
-    X = getDataFromColumn(data, 0)
-    ## Create Features Array with specified number of features
-    for i in range(n):
-        if i > 0:
-            temp = getDataFromColumn(data, i)
-            X = np.column_stack((X, temp))
-    return X
-
-# will return a dictionary with the accuracy and standard deviation of the specified model
-def getCrossValAccuracy(model, X, Y, cv):
-    scores = cross_val_score(model, X, Y, cv=cv, scoring='accuracy')
-    average_accuracy = scores.mean()
-    average_accuracy_std = scores.std()
-
-    return {
-        "acc": average_accuracy,
-        "std": average_accuracy_std
-    }
-
-##
-## --------------------------------
-##          ML METHODS
-##---------------------------------
-##
-
-def logRegression(features, targets, poly):
-    ## Get Features and Target Values
-    X = features
-    Y = targets
-
-    ## Check for Polynomial Features
-    if poly == 0 or None:
-        model = LogisticRegression().fit(X, Y)
-    else:
-        polynomials = PolynomialFeatures(poly)
-        XPoly = polynomials.fit_transform(X)
-        model = LogisticRegression().fit(XPoly, Y)
-    
-    return model
-    
-## BROKEN NEEDS TO BE FIXED
-def lasso(features, targets, poly, C):
-    ## Get Features and Target Values
-    X = features
-    Y = targets
-
-    ## Check for Polynomial Features
-    if poly == 0 or None:
-        model = Lasso(alpha=1/(2 * C)).fit(X, Y)
-    else:
-        polynomials = PolynomialFeatures(poly)
-        XPoly = polynomials.fit_transform(X)
-        model = Lasso(alpha=1/(2 * C)).fit(XPoly, Y)
-
-    return model
-
-## BROKEN NEEDS TO BE FIXED
-def ridge(features, targets, poly, C):
-    ## Get Features and Target Values
-    X = features
-    Y = targets
-    
-    ## Check for Polynomial Features
-    if poly == 0 or None:
-        model = Ridge(alpha=1/(2 * C)).fit(X, Y)
-    else:
-        polynomials = PolynomialFeatures(poly)
-        XPoly = polynomials.fit_transform(X)
-        model = Ridge(alpha=1/(2 * C)).fit(XPoly, Y)
-
-    return model
-
-def kNN(features, targets, neighbours):
-    ## Get Features and Target Values
-    X = features
-    Y = targets
-    
-    model = KNeighborsClassifier(n_neighbors=neighbours).fit(X, Y)
-    return model
-
-## BROKEN NEEDS TO BE FIXED
-def dummy(features, targets, type):
-    ## Get Features and Target Values
-    X = np.array(features).reshape(-1, 1)
-    Y = np.array(targets).reshape(-1, 1)
-
-    model = DummyRegressor(strategy=type).fit(X, Y)
-    return model
+'''
+Utility Functions
+'''
 
 
-##
-## --------------------------------
-##         MAIN EXECUTION
-##---------------------------------
-##
+# Returns a list of the dataset features
+# Parameters:
+#    data: Each row in the dataset.
+def get_features(data, n, start=0):
+    return np.array([x[start:n] for x in data if len(x) == len(data[0]) and x[len(x) - 1] != 0])
 
-df = pd.read_csv('dataset/challenger_games/challenger_games.euw1.csv').values.tolist()
 
-X = getFeatures(df, len(df[0]) - 2)
-Y = getDataFromColumn(df, len(df[0]) - 1)
-model = ridge(X, Y, 2, 100)
+# Returns a list of the dataset targets
+# Parameters:
+#   data: Each row in the dataset. Function assumes that the final element in a row is the target.
+def get_targets(data):
+    return np.array([x[len(x) - 1] for x in data if len(x) == len(data[0]) and x[len(x) - 1] != 0])
 
-scores = getCrossValAccuracy(model, X, Y, 10)
 
-print(scores["acc"])
+'''
+Cross Validating Functions
+'''
+
+
+def cross_validate_baseline_random_score(features, targets):
+    scores = cross_val_score(DummyClassifier(strategy='uniform'), features, targets, cv=5, scoring='accuracy')
+    return {'mean': scores.mean(), 'std': scores.std()}
+
+
+def cross_validate_baseline_most_frequent_score(features, targets):
+    scores = cross_val_score(DummyClassifier(strategy='most_frequent'), features, targets, cv=5, scoring='accuracy')
+    return {'mean': scores.mean(), 'std': scores.std()}
+
+
+def cross_validate_logistic_regression(features, targets):
+    C_range = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+
+    means, stds, models = [], [], []
+
+    for C in C_range:
+        scores = cross_val_score(LogisticRegression(C=C, penalty='l2', solver='lbfgs'), features, targets, cv=5,
+                                 scoring='accuracy')
+        means.append(scores.mean())
+        stds.append(scores.std())
+
+    br = cross_validate_baseline_random_score(features, targets)
+    bf = cross_validate_baseline_most_frequent_score(features, targets)
+
+    plt.figure(figsize=(10, 10))
+    plt.errorbar(C_range, [br['mean']] * len(C_range), yerr=[br['std']] * len(C_range), fmt='y')
+    plt.errorbar(C_range, [bf['mean']] * len(C_range), yerr=[bf['std']] * len(C_range), fmt='r')
+    plt.errorbar(C_range, means, yerr=stds, fmt='b')
+    plt.ylabel('Mean Accuracy Score')
+    plt.xlabel('C')
+    plt.xscale('log')
+    plt.title('Comparing accuracy of baseline models with Logistic Regression model')
+    plt.legend(['Baseline (random)', 'Baseline (most frequent)', 'Trained model'])
+    plt.show()
+
+    print('Mean Accuracy Scores for Logistic Regression')
+    print(means)
+
+
+'''
+Model Training Functions
+'''
+
+
+def confusion_matrix_random(features, targets):
+    X_train, X_test, Y_train, Y_test = train_test_split(features, targets, test_size=0.2)
+    model = DummyClassifier(strategy='uniform').fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+    print('Confusion Matrix for Baseline Classifier (Random)')
+    print(confusion_matrix(Y_test, Y_pred))
+    return confusion_matrix(Y_test, Y_pred)
+
+
+def confusion_matrix_most_frequent(features, targets):
+    X_train, X_test, Y_train, Y_test = train_test_split(features, targets, test_size=0.2)
+    model = DummyClassifier(strategy='most_frequent').fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+    print('Confusion Matrix for Baseline Classifier (Most Frequent)')
+    print(confusion_matrix(Y_test, Y_pred))
+    return confusion_matrix(Y_test, Y_pred)
+
+
+def logistic_regression(features, targets):
+    X_train, X_test, Y_train, Y_test = train_test_split(features, targets, test_size=0.2)
+    model = LogisticRegression(C=1, penalty='l2', solver='lbfgs').fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+    print('Confusion Matrix for Baseline Classifier (Most Frequent)')
+    print(confusion_matrix(Y_test, Y_pred))
+
+    cm_rand = confusion_matrix_random(features, targets)
+    cm_freq = confusion_matrix_most_frequent(features, targets)
+
+    fpr, tpr, _ = roc_curve(Y_test, model.decision_function(X_test))
+    plt.plot(fpr, tpr)
+    rand_tpr = cm_rand[1][1] / (cm_rand[1][1] + cm_rand[1][0])
+    rand_fpr = cm_rand[0][1] / (cm_rand[0][1] + cm_rand[0][0])
+    freq_tpr = cm_freq[1][1] / (cm_freq[1][1] + cm_freq[1][0])
+    freq_fpr = cm_freq[0][1] / (cm_freq[0][1] + cm_freq[0][0])
+    plt.plot(rand_tpr, rand_fpr, 'rs')
+    plt.plot(freq_tpr, freq_fpr, 'gs')
+    plt.title('ROC Curve for Logistic Regression Model')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.legend(['Trained model', 'Baseline (random)', 'Baseline (most frequent)'])
+    plt.show()
+
+
+'''
+Main
+'''
+
+
+def main():
+    games = pd.read_csv('dataset/challenger_games/challenger_games_big.euw1.csv').values.tolist()
+    X = get_features(games, len(games[0]) - 1)
+    Y = get_targets(games)
+
+    cross_validate_logistic_regression(X, Y)
+    confusion_matrix_random(X, Y)
+    confusion_matrix_most_frequent(X, Y)
+    logistic_regression(X, Y)
+
+
+if __name__ == "__main__":
+    main()
